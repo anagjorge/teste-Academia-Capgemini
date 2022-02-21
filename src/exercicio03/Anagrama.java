cef15f6993ea53cae237fbb85566366e65ecc2dc
Ana Jorge <69881067+anagjorge@users.noreply.github.com>
2022-02-21 00:17:18 -0300

    first commit

--- src/exercicio03/Anagrama.java
+++ src/exercicio03/Anagrama.java
@@ -1,5 +1,26 @@
 package exercicio03;
 
-public class Anagrama {
+import java.util.Scanner;
 
+public class Anagrama {
+	
+	public static void isAnagrama(String palavra) {
+		int n = 0;
+         if(palavra.length() > 1) {		
+			for (int i = 0; i < palavra.length(); i++) {
+				String depois = palavra.substring(i, i + 1);
+				isAnagrama( depois);
+				n++;
+			}
+	     	System.out.println(""+n);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Digite uma palavra");
+		String palavra = leitor.nextLine();	
+		isAnagrama(palavra);
+	}
 }
